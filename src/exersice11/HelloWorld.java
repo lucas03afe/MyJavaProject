86d0ba21a6f71925708b5261949a2531f6cff2e7
Lucas <lucas03afe@gmail.com>
2024-11-12 23:14:30 +0100

    Hello Master

--- src/exersice11/HelloWorld.java
+++ src/exersice11/HelloWorld.java
@@ -1,10 +1,8 @@
 package exersice11;
 
-import java.util.Scanner;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Master!");
     }
 }
